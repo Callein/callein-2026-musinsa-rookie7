41acd5b3835687db63f94c781e987f706ed997be
callein <callendev20@gmail.com>
2026-02-08 15:38:41 +0900

    feat(models): export all domain models
    - Expose Course, CourseSchedule, Department, Enrollment, Professor, Student
    - Simplify imports across the application

---
+++ src/models/__init__.py
@@ -0,0 +1,15 @@
+from src.models.department import Department
+from src.models.professor import Professor
+from src.models.course import Course
+from src.models.course_schedule import CourseSchedule
+from src.models.student import Student
+from src.models.enrollment import Enrollment
+
+__all__ = [
+    "Department",
+    "Professor",
+    "Course",
+    "CourseSchedule",
+    "Student",
+    "Enrollment",
+]
